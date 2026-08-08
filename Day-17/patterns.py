95dde44d4cc9f03ecc5cc17365f288c8cbb619fc
Anjana Andurthi <anjanaandurthi@gmail.com>
2026-08-08 10:55:21 +0530

    Add files via upload

---
+++ Day-17/patterns.py
@@ -0,0 +1,14 @@
+#alphabet V pattern
+
+'''
+
+n=int(input("Enter the size: ")) 
+m=n//2
+for i in range(n):
+    for j in range(n):
+        if(j==0 and i<=m or j==n-1 and i<=m or i-j==m and i>=m or i+j==m+n-1 and i>=m):
+            print('*',end=' ')   
+        else:
+            print(' ',end=' ') 
+    print()
+'''    
